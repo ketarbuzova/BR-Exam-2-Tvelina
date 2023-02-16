d5aa27e5c71289f6d90229231c4274954787097d
kateryna tvelina <kearbuzova@gmail.com>
2023-02-16 20:47:26 +0200

    smth

--- src/js/index.js
+++ src/js/index.js
@@ -183,35 +183,3 @@ formBtn.addEventListener('click', (e) => {
     setTimeout(() => { formBtn.value = 'Submit'; }, 1500);
   }
 });
-
-
-// const form = document.querySelector("form");
-// const email = document.getElementById("email");
-
-// email.addEventListener("input", (event) => {
-//   if (email.validity.valid) {
-//     emailError.textContent = ""; 
-//     emailError.className = "error"; 
-//   } else {
-//     showError();
-//   }
-// });
-
-// form.addEventListener("submit", (event) => {
-//   if (!email.validity.valid) {
-//     showError();
-//     event.preventDefault();
-//   }
-// });
-
-// function showError() {
-//   if (email.validity.valueMissing) {
-//     emailError.textContent = "You need to enter an email address.";
-//   } else if (email.validity.typeMismatch) {
-//     emailError.textContent = "Entered value needs to be an email address.";
-//   } else if (email.validity.tooShort) {
-//     emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
-//   }
-
-//   emailError.className = "error active";
-// }
